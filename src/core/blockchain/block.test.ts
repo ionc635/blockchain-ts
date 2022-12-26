061e95be764bc5d2a69f4d3c0e055ebb3f0c4f6b
steve <ionc635@naver.com>
2022-12-26 15:54:56 +0900

    test: 테스트 세팅 및 단위 테스트 작성

---
+++ src/core/blockchain/block.test.ts
@@ -0,0 +1,26 @@
+import { Block } from "@core/blockchain/block";
+import { GENESIS } from "@core/config";
+
+describe("Block 검증", () => { 
+    let newBlock: Block;
+
+    it("블록 생성 테스트", () => {
+        const data = ["Block #2"];
+
+        newBlock = Block.generateBlock(GENESIS, data);
+
+        console.log(newBlock)
+    })
+
+    it("블록 검증 테스트", () => {
+        const isValidBlock = Block.isValidNewBlock(newBlock, GENESIS);
+
+        if (isValidBlock.isError) {
+            console.error(isValidBlock.error);
+            return expect(true).toBe(false);
+        }
+
+        expect(isValidBlock.isError).toBe(false);
+    })
+})
+
